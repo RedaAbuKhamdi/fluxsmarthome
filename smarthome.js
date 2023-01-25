7a0d53f86b5a995093039350fce8551ef3c0f848
Reda <rabouhamdi@gmail.com>
2023-01-25 22:59:04 +0200

    Added test path

--- smarthome.js
+++ smarthome.js
@@ -6,6 +6,9 @@ const port = 3000;
 app.get('/v1.0', (req, res) => {
   res.sendStatus(200);
 });
+app.get('/test', (req, res) => {
+  res.send("Echo test");
+});
 
 app.listen(port,()=>{
     console.log("App listening");
